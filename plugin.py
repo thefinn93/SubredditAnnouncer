9bd57cd6a26100023e401c9ecea357e8d686caeb
Finn <finn@finn.io>
2016-04-27 13:42:24 -0700

    Style cleanup. Remove unused imports, etc

--- plugin.py
+++ plugin.py
@@ -28,10 +28,7 @@
 
 ###
 
-import supybot.utils as utils
 from supybot.commands import *
-import supybot.plugins as plugins
-import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
 import supybot.ircmsgs as ircmsgs
 import supybot.ircdb as ircdb
@@ -42,30 +39,35 @@ import supybot.conf as conf
 import requests
 import ConfigParser
 import json
-import re
 
 _ = PluginInternationalization('SubredditAnnouncer')
+
 
 @internationalizeDocstring
 class SubredditAnnouncer(callbacks.Plugin):
     """Add the help for "@plugin help SubredditAnnouncer" here
     This should describe *how* to use this plugin."""
+
     pass
-    
+
     def __init__(self, irc):
         self.__parent = super(SubredditAnnouncer, self)
         self.__parent.__init__(irc)
         self.savefile = conf.supybot.directories.data.dirize("subredditAnnouncer.db")
-        self.headers = {"User-Agent":"SubredditAnnouncer (thefinn93@thefinn93.com)"}
-                
+        self.headers = {"User-Agent": "SubredditAnnouncer (finn@finn.io)"}
+
         def checkForPosts():
             self.checkReddit(irc)
         try:
-            schedule.addPeriodicEvent(checkForPosts, self.registryValue('checkinterval')*60, 'redditCheck', False)
+            schedule.addPeriodicEvent(checkForPosts,
+                                      self.registryValue('checkinterval')*60,
+                                      'redditCheck', False)
         except AssertionError:
             schedule.removeEvent('redditCheck')
-            schedule.addPeriodicEvent(checkForPosts, self.registryValue('checkinterval')*60, 'redditCheck', False)
-    
+            schedule.addPeriodicEvent(checkForPosts,
+                                      self.registryValue('checkinterval')*60,
+                                      'redditCheck', False)
+
     def post(self, irc, channel, msg):
         try:
             irc.queueMsg(ircmsgs.privmsg(channel, str(msg)))
@@ -73,16 +75,16 @@ class SubredditAnnouncer(callbacks.Plugin):
             self.log.warning("Failed to send to " + channel + ": " + str(type(e)))
             self.log.warning(str(e.args))
             self.log.warning(str(e))
-    
+
     def checkReddit(self, irc):
         try:
             data = json.load(open(self.savefile))
-        except Exception as inst:
+        except Exception:
             domain = "http://www.reddit.com"
             if self.registryValue('domain') is not "":
-                doman = self.registryValue('domain')
-            data = {domain: {"announced":[],"subreddits":[]}}
-            
+                domain = self.registryValue('domain')
+            data = {domain: {"announced": [], "subreddits": []}}
+
         parser = ConfigParser.SafeConfigParser()
         parser.read([self.registryValue('configfile')])
         for channel in parser.sections():
@@ -93,13 +95,13 @@ class SubredditAnnouncer(callbacks.Plugin):
                     domain = self.registryValue('domain')
                     if parser.has_option(channel, 'domain'):
                         domain = parser.get(channel, 'domain')
-                    if not domain in data:
-                        data[domain] = {"announced":[],"subreddits":[]}
+                    if domain not in data:
+                        data[domain] = {"announced": [], "subreddits": []}
                         self.log.info("Creating data store for " + domain)
                     messageformat = "[NEW] [{redditname}] [/r/{subreddit}] {bold}{title}{bold} - {shortlink}"
                     if parser.has_section("global"):
-                        if parser.has_option("global","format"):
-                            messageformat = parser.get("global","format")
+                        if parser.has_option("global", "format"):
+                            messageformat = parser.get("global", "format")
                     if parser.has_option(channel, "format"):
                         messageformat = parser.get(channel, "format")
                     url = domain + "/r/" + sub + "/new.json?sort=new"
@@ -109,49 +111,51 @@ class SubredditAnnouncer(callbacks.Plugin):
                     for post in listing['data']['children']:
                         if not post['data']['id'] in data[domain]['announced']:
                             shortlink = self.registryValue('domain') + "/" + post['data']['id']
-                            if self.registryValue('shortdomain') != None:
-                                shortlink = self.registryValue('shortdomain') + "/" + post['data']['id']
-                                
+                            if self.registryValue('shortdomain') is not None:
+                                shortlink = self.registryValue('shortdomain') + "/"
+                                shortlink += post['data']['id']
+
                             if parser.has_option(channel, 'shortdomain'):
-                                shortlink = parser.get(channel, 'shortdomain') + "/" + post['data']['id']
-                            
+                                shortlink = parser.get(channel, 'shortdomain') + "/"
+                                shortlink += post['data']['id']
+
                             redditname = ""
                             if self.registryValue('redditname') is not "":
                                 redditname = self.registryValue('redditname')
-                                
+
                             if parser.has_option(channel, 'redditname'):
                                 redditname = parser.get(channel, 'redditname')
-                                
+
                             if post['data']['subreddit'] in data[domain]['subreddits']:
-                                msg = messageformat.format(redditname = redditname,
-                                    subreddit = post['data']['subreddit'].encode('utf-8').strip(),
-                                    title = post['data']['title'].encode('utf-8').strip(),
-                                    author = post['data']['author'].encode('utf-8').strip(),
-                                    link = post['data']['url'].encode('utf-8').strip(),
-                                    shortlink = shortlink,
-                                    score = post['data']['score'].encode('utf-8').strip(),
-                                    ups = post['data']['ups'].encode('utf-8').strip(),
-                                    downs = post['data']['downs'].encode('utf-8').strip(),
-                                    comments = post['data']['num_comments'].encode('utf-8').strip(),
-                                    domain = domain,
-                                    bold = chr(002),
-                                    underline = "\037",
-                                    reverse = "\026",
-                                    white = "\0030",
-                                    black = "\0031",
-                                    blue = "\0032",
-                                    red = "\0034",
-                                    dred = "\0035",
-                                    purple = "\0036",
-                                    dyellow = "\0037",
-                                    yellow = "\0038",
-                                    lgreen = "\0039",
-                                    dgreen = "\00310",
-                                    green = "\00311",
-                                    lpurple = "\00313",
-                                    dgrey = "\00314",
-                                    lgrey = "\00315",
-                                    close = "\003")
+                                msg = messageformat.format(redditname=redditname,
+                                    subreddit=post['data']['subreddit'].encode('utf-8').strip(),
+                                    title=post['data']['title'].encode('utf-8').strip(),
+                                    author=post['data']['author'].encode('utf-8').strip(),
+                                    link=post['data']['url'].encode('utf-8').strip(),
+                                    shortlink=shortlink,
+                                    score=post['data']['score'].encode('utf-8').strip(),
+                                    ups=post['data']['ups'].encode('utf-8').strip(),
+                                    downs=post['data']['downs'].encode('utf-8').strip(),
+                                    comments=post['data']['num_comments'].encode('utf-8').strip(),
+                                    domain=domain,
+                                    bold=chr(002),
+                                    underline="\037",
+                                    reverse="\026",
+                                    white="\0030",
+                                    black="\0031",
+                                    blue="\0032",
+                                    red="\0034",
+                                    dred="\0035",
+                                    purple="\0036",
+                                    dyellow="\0037",
+                                    yellow="\0038",
+                                    lgreen="\0039",
+                                    dgreen="\00310",
+                                    green="\00311",
+                                    lpurple="\00313",
+                                    dgrey="\00314",
+                                    lgrey="\00315",
+                                    close="\003")
                                 self.post(irc, channel, msg)
                             else:
                                 self.log.info("Not posting " + self.registryValue('shortdomain') + "/" + post['data']['id'] + " because it's our first time looking at /r/" + post['data']['subreddit'])
@@ -160,18 +164,18 @@ class SubredditAnnouncer(callbacks.Plugin):
                             data[domain]['announced'].append(post['data']['id'])
                 except Exception as e:
                     self.log.warning("Whoops! Something fucked up: " + str(e))
-                if not domain in data:
-                    data[domain] = {"announced":[],"subreddits":[]}
+                if domain not in data:
+                    data[domain] = {"announced": [], "subreddits": []}
                     self.log.info("Creating data store for " + domain)
-                if not sub in data[domain]['subreddits']:
+                if sub not in data[domain]['subreddits']:
                     data[domain]['subreddits'].extend(addtoindex)
         savefile = open(self.savefile, "w")
         savefile.write(json.dumps(data))
         savefile.close()
-    
+
     def check(self, irc, msg, args):
         """takes no args
-                
+
         Checks the specified subreddit and announces new posts"""
         if ircdb.checkCapability(msg.prefix, "owner"):
             irc.reply("Checking!")
@@ -179,7 +183,7 @@ class SubredditAnnouncer(callbacks.Plugin):
         else:
             irc.reply("Fuck off you unauthorized piece of shit")
     check = wrap(check)
-    
+
     def start(self, irc, msg, args):
         """takes no arguments
 
@@ -189,7 +193,9 @@ class SubredditAnnouncer(callbacks.Plugin):
             def checkForPosts():
                 self.checkReddit(irc)
             try:
-                schedule.addPeriodicEvent(checkForPosts, self.registryValue('checkinterval')*60, 'redditCheck', False)
+                schedule.addPeriodicEvent(checkForPosts,
+                                          self.registryValue('checkinterval')*60,
+                                          'redditCheck', False)
             except AssertionError:
                 irc.reply('The reddit checker was already running!')
             else:
@@ -197,7 +203,7 @@ class SubredditAnnouncer(callbacks.Plugin):
         else:
             irc.reply("Fuck off you unauthorized piece of shit")
     start = wrap(start)
-    
+
     def stop(self, irc, msg, args):
         """takes no arguments
 
